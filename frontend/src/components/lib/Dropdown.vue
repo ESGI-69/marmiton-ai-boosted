43bee791ecd7940a0a20d1b5157811020a453e45
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2023-09-27 23:53:54 +0200

    feat: added dropdown

---
+++ frontend/src/components/lib/Dropdown.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="dropdown"
+    @click="toggle"
+  >
+    <slot name="toggler">
+      <button>Toggle</button>
+    </slot>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { ref, provide } from 'vue';
+const active = ref(false);
+const toggle = () => {
+  active.value = !active.value;
+};
+provide('active', active);
+</script>
+
+<style>
+.dropdown {
+    position: relative;
+    display: inline;
+}
+</style>
+
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,10 +6,26 @@
     </h1>
     <SearchBar />
     <Filters />
+    <div>
+      <span>Home > Breadcrumb</span>
+      <Dropdown>
+        <template #toggler>
+          <button>Menu</button>
+        </template>
+        <DropdownList>
+          <DropdownListItem>Test</DropdownListItem>
+          <DropdownListItem>Test</DropdownListItem>
+          <DropdownListItem>Test</DropdownListItem>
+        </DropdownList>
+      </Dropdown>
+    </div>
   </main>
 </template>
 
 <script setup>
 import Filters from '@/components/Filters.vue';
 import SearchBar from '@/components/lib/SearchBar.vue';
+import Dropdown from '@/components/lib/Dropdown.vue';
+import DropdownList from '@/components/lib/DropdownList.vue';
+import DropdownListItem from '@/components/lib/DropdownListItem.vue';
 </script>
