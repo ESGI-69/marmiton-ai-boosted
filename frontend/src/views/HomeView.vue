3fd19ed1729fe41a6155c0730ddca1aa04f4dd56
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2023-09-28 01:12:27 +0200

    feat: added start rating

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -17,6 +17,7 @@
           <DropdownListItem>Test</DropdownListItem>
         </DropdownList>
       </Dropdown>
+      <StarRating :rating="3" />
     </div>
   </main>
 </template>
@@ -29,4 +30,5 @@ import DropdownList from '@/components/lib/DropdownList.vue';
 import DropdownListItem from '@/components/lib/DropdownListItem.vue';
 import Button from '@/components/lib/Button.vue';
 import Breadcrumb from '@/components/lib/Breadcrumb.vue';
+import StarRating from '@/components/lib/StarRating.vue';
 </script>
