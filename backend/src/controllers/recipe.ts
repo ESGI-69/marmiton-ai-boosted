9fc802392c4513362dce8220753a6ba89d86e2af
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2023-09-29 19:35:09 +0200

    feat: added search prisma engine

--- backend/src/controllers/recipe.ts
+++ backend/src/controllers/recipe.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import recipeSerice from '../service/recipe';
 import checkParams from '../utils/checkParams';
+import checkQueryParams from '../utils/checkQueryParams';
 
 export default {
   get: async (req: Request, res: Response, next: NextFunction) => {
@@ -13,4 +14,15 @@ export default {
       next(error);
     }
   },
+  search: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      checkQueryParams(['title', 'description'], req.query);
+      const { title, description } = req.query as { title?: string; description?: string; };
+      const recipes = await recipeSerice.search({ title: title || '', description: description || '' });
+      res.status(200).json(recipes);
+    } catch (error) {
+      next(error);
+    }
+  },
+
 };
