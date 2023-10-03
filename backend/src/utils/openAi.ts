a44f479c292ecb70319b5860589774abafe0596b
timdev0 <quentin.peltier.pro@gmail.com>
2023-10-03 15:27:19 +0200

    feat(search-ai): back to simple prompt generation

--- backend/src/utils/openAi.ts
+++ backend/src/utils/openAi.ts
@@ -3,16 +3,12 @@ import OpenAI, { ClientOptions } from 'openai';
 class OpenAIQueryBuilder {
   private static instance: OpenAIQueryBuilder;
   private client: OpenAI;
-  private previousResponse: string | null;
-  private messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[];
 
   private constructor() {
     const apiKey = process.env.OPENAI_API_KEY || '';
-    // const dangerouslyAllowBrowser = process.env.OPENAI_DANGEROUSLY_ALLOW_BROWSER === 'true';
-    const clientOptions: ClientOptions = { apiKey };
+    const dangerouslyAllowBrowser = process.env.OPENAI_DANGEROUSLY_ALLOW_BROWSER === 'true';
+    const clientOptions: ClientOptions = { apiKey, dangerouslyAllowBrowser };
     this.client = new OpenAI(clientOptions);
-    this.previousResponse = null;
-    this.messages = [];
   }
 
   public static getInstance(): OpenAIQueryBuilder {
@@ -23,34 +19,21 @@ class OpenAIQueryBuilder {
   }
 
   public async generatePrompt(prompt: string, systemMessage: string, model: string): Promise<OpenAI.Chat.Completions.ChatCompletion.Choice[]> {
-    if (!this.messages.some((message) => message.content === systemMessage)) {
-      this.messages.push({
+    const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] =  [
+      {
         role: 'system',
         content: systemMessage,
-      });
-    }
-
-    if (this.previousResponse) {
-      this.messages.push({
-        role: 'assistant',
-        content: this.previousResponse,
-      });
-    }
-
-    this.messages.push({
-      role: 'user',
-      content: prompt,
-    });
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ];
 
     const response = await this.client.chat.completions.create({
-      messages: this.messages,
+      messages,
       model,
     });
-
-    const message = response.choices[0]?.message;
-    if (message && message.content) {
-      this.previousResponse = message.content;
-    }
 
     return response.choices;
   }
