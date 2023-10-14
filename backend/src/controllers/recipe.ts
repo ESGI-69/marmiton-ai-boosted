53f5762d5801d2027f6c377a3ed3526469a70fe9
mrpinkcat <gatien.anizan@gmail.com>
2023-10-14 15:51:45 +0200

    fix: typo

--- backend/src/controllers/recipe.ts
+++ backend/src/controllers/recipe.ts
@@ -34,8 +34,8 @@ export default {
       const systemMessage = 'Tu es un chef étoilé au guide michelin et tu as une quinzaine d\'années d\'expérience dans le métier avec plusieurs concours culinaires gagnés à l\'internationnal, ton but est de donner des recettes. Formattez la recette de cuisine et répondez uniquement au format JSON. Ce JSON devrai contenir les champs title (string), description (string), ingredientsWithQuantities (array of object) with quantity (string), name (string) , steps (array). Répond moi en français et ne répéte jamais le nom des ingrédients dans la quantité';
       const model = 'gpt-3.5-turbo';
       const openAiResponse = await openAiQueryBuilder.generatePrompt(prompt, systemMessage, model);
-      const recipie = await recipeSerice.create(JSON.parse(openAiResponse.choices[0].message.content as string));
-      res.status(200).send(recipie);
+      const recipe = await recipeSerice.create(JSON.parse(openAiResponse.choices[0].message.content as string));
+      res.status(200).send(recipe);
     } catch (error) {
       next(error);
     }
