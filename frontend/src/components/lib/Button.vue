690eb92fc503bb581705d766f411dce0669db585
mrpinkcat <gatien.anizan@gmail.com>
2023-10-16 02:13:36 +0200

    fix(Button): wrong indent

--- frontend/src/components/lib/Button.vue
+++ frontend/src/components/lib/Button.vue
@@ -25,29 +25,29 @@ defineProps({
 
 <style lang="scss" scoped>
 .button {
-    background: var(--color-primary);
-    color: var(--color-white);
-    border: 1px solid transparent;
-    border-radius: 4px;
-    padding: var(--space-1) var(--space-4);
-    font-weight: 500;
-    cursor: pointer;
+  background: var(--color-primary);
+  color: var(--color-white);
+  border: 1px solid transparent;
+  border-radius: 4px;
+  padding: var(--space-1) var(--space-4);
+  font-weight: 500;
+  cursor: pointer;
 
+  &:hover {
+    background: var(--color-text);
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+
+  &--light {
+    background: var(--color-white);
+    color: var(--color-text);
+    border: 1px solid var(--color-border);
     &:hover {
-        background: var(--color-text);
+      background: var(--color-border);
     }
-
-    &:disabled {
-        cursor: not-allowed;
-    }
-
-    &--light {
-        background: var(--color-white);
-        color: var(--color-text);
-        border: 1px solid var(--color-border);
-        &:hover {
-            background: var(--color-border);
-        }
-    }
+  }
 }
 </style>
