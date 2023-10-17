e5b8e3ad37d1a280a0091bee1eb7b4cf30a600d0
timdev0 <quentin.peltier.pro@gmail.com>
2023-10-17 12:42:11 +0200

    feat(recipe-comments): add Comment component

---
+++ frontend/src/components/CommentCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="comment-card">
+    <img
+      class="comment-card__profile-picture"
+      :src="imageUrl"
+      alt="profile-picture"
+    >
+    <div class="comment-card__container">
+      <p>{{ name }}</p>
+      <p>{{ comment }}</p>
+      <MultiStarsRating
+        :model-value="notation"
+        :text-visable="false"
+        class="comment-card__stars"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import MultiStarsRating from '@/components/lib/MultiStarsRating.vue';
+
+const { notation, name, comment } = defineProps({
+  notation: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    default: 'http://placekitten.com/300/300',
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.comment-card {
+  display: flex;
+  flex-direction: row;
+  padding: var(--space-4);
+  gap: var(--space-4);
+  background-color: var(--color-white);
+  width: 263px;
+  border-radius: var(--space-4);
+
+  &__profile-picture {
+    border-radius: var(--space-8);
+    width: var(--space-8);
+    height: var(--space-8);
+  }
+
+  &__container {
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-4);
+    align-items: flex-start;
+  }
+  &__container > *:last-child {
+    margin-top: auto;
+  }
+}
+</style>
