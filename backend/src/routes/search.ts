5c5ae05a141a5de8901d7c76a071f57a4c49eb79
timdev0 <quentin.peltier.pro@gmail.com>
2023-12-14 22:50:01 +0100

    feat(search.Ts): remove

--- backend/src/routes/search.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import searchController from '../controllers/search';
-
-const router = Router();
-
-router.post('/', searchController.post);
-
-export default router;
